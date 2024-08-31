a31cb9a4f74be735811e4be54d18368a57ee4e78
ivanrotela <167491223+ivanrotela@users.noreply.github.com>
2024-08-31 13:52:40 -0300

    p5 no terminado

---
+++ P5_js.js
@@ -0,0 +1,18 @@
+function preload() {
+  foto = loadImage('data/obra.jpeg');
+
+  
+
+}
+function setup() {
+createCanvas(800,400);
+}
+
+
+function draw() {
+image(foto,0,0);
+ for(i=0; i<6; i++){
+ for(j=0; j<6; j++){
+  dibujarcuadrado(400+66*i,66*j);
+
+}
